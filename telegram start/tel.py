9e1e057fae79bf8799284210a5f34d42a442b78b
Artem Moiseev <rt.rk.ra@gmail.com>
2023-04-09 21:44:31 +0300

    Изменил еще раз

--- telegram start/tel.py
+++ telegram start/tel.py
@@ -1,7 +1,32 @@
 import requests
+
+
 query = {
     'posts' : requests.get("https://jsonplaceholder.typicode.com/posts").json(),
     'comments' : requests.get("https://jsonplaceholder.typicode.com/comments").json()
 }
-print(query['posts'][0]['userId'])
+
+
+def Answer(b):
+    comment = []
+    post = []
+    for i in range(len(query['posts'])):
+        if query['posts'][i]['userId'] == b:
+                post.extend(query['posts'][i].items())
+        for j in range(len(query['comments'])):
+            if query['comments'][j]['postId'] == query['posts'][b]['id']:
+                    comment.extend(query['comments'][j].items())
+
+    answer = {'comment' : comment,
+            'post' : post}
+
+    return answer
+
+def User(a):
+     user = {'userId' : Answer(a)}
+     return user
+
+  
+a = User(2)
+print(a)
 
